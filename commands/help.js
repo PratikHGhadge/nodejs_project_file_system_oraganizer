0b1b8de25daab1caa615acfbb02a287437706b7c
Pratik <pratikhg.2001@gmail.com>
2022-05-07 17:26:12 +0530

    tree and global functionality implemented

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+function helpFun(dirPath) {
+    console.log(`List of All the command 
+       Node main.js tree "directoripath"
+       Node main.js organize "directorypath"
+       Node main.js help
+    `);
+
+}
+
+module.exports = {
+    helpKey : helpFun
+}
